a11d56d91d1b4b02b23a55a9e7bcea800e511c13
raashed <mohammedraashed10@gmail.com>
2025-08-15 16:52:16 +0530

    chore: ping-pong game was added

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("server started!")
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port: 8080});
+
+wss.on('connection', function(socket){
+    console.log('server connected!!');
+    socket.on('message', (e) => {
+        if(e.toString() === 'ping'){
+            socket.send('pong')
+        }
+    })
+})
