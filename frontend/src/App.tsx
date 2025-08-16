34b574ee1f1e298279bc7ecb3cd23cb4ac461ed9
raashed <mohammedraashed10@gmail.com>
2025-08-16 10:25:33 +0530

    chore: frontend was completed!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useRef, useState } from "react"
+
 function App() {
+
+  const [socket, setSocket] = useState();
+  const inputRef = useRef<HTMLInputElement>();
+
+  function sendMessage(){
+    if(!socket){
+      return;
+    }
+
+    const message = inputRef.current.value;
+    
+    //@ts-ignore
+    socket.send(message);
+  }
+
+  useEffect(() => {
+    const wss = new WebSocket("ws://localhost:8080");
+    setSocket(wss)
+
+    wss.onmessage = (e) => {
+      alert(e.data);
+    }
+  }, []);
+
   return (
-    <div className="bg-red-100 text-9xl">
-      tailwind check!!
+    <div>
+      <input type="text" placeholder="message" ref={inputRef}/>
+      <button onClick={sendMessage}>send</button>
     </div>
   )
 }
